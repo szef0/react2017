4bbbec6be8e7998a2b183c07072d543f78d0d9d7
Jonathan Foster <jxf9099@rit.edu>
2017-02-08 16:55:24 -0500

    add react router

---
+++ src/app/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        &copy; 2017 fostertheweb
+      </footer>
+    )
+  }
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+
+import App from './app/App'
+import Home from './Home'
+
+const routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <Route path='/home' component={Home} />
+    </Route>
+  </Router>
+)
 
 ReactDOM.render(
-  <App />,
+  routes,
   document.getElementById('root')
-);
+)
